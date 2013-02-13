1e6f04a81867072538806caf9c0e5eca1823c4b5
Matt Bierbaum <matt.bierbaum@gmail.com>
2013-02-13 11:20:40 -0500

    tracking again

--- entbody.js
+++ entbody.js
@@ -1,13 +1,3 @@
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-38463781-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
 // all of the global variables for dynamics
 var x=[];
 var y=[];
